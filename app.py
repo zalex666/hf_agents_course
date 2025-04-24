148ab21a10df3bbd811fdb07eb89515464a7e214
Joffrey THOMAS <Jofthomas@users.noreply.huggingface.co>
2025-04-24 11:18:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -143,10 +143,19 @@ def run_and_submit_all( profile: gr.OAuthProfile | None):
 # --- Build Gradio Interface using Blocks ---
 with gr.Blocks() as demo:
     gr.Markdown("# Basic Agent Evaluation Runner")
-    gr.Markdown(
-        "Please clone this space, then modify the code to define your agent's logic within the `BasicAgent` class. "
-        "Log in to your Hugging Face account using the button below. This uses your HF username for submission. "
-        "Click 'Run Evaluation & Submit All Answers' to fetch questions, run your agent, submit answers, and see the score."
+     gr.Markdown(
+        """
+        **Instructions:**
+
+        1.  Please clone this space, then modify the code to define your agent's logic within the `BasicAgent` class or outside if necessary.
+        2.  Log in to your Hugging Face account using the button below. This uses your HF username for submission.
+        3.  Click 'Run Evaluation & Submit All Answers' to fetch questions, run your agent, submit answers, and see the score.
+
+        ---
+        **Disclaimers:**
+        Once clicking on the "submit button, it can take quite some time ( this is the time for the agent to go through all the questions).
+        This space provides a basic setup and is intentionally sub-optimal to encourage you to develop your own, more robust solution. For instance for the delay process of the submit button, a solution could be to cache the answers and submit in a seperate action or even to answer the questions in async.
+        """
     )
 
     gr.LoginButton()
